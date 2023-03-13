fa356b6b8db1de401401a82338732fa2a3398a39
shi0n0 <shiono.yui1080@gmail.com>
2023-03-13 19:57:53 +0900

    外部APIを使用してランダムな単語に変更

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shi0n0/typing_game
 
 go 1.19
+
+require github.com/Pallinder/go-randomdata v1.2.0 // indirect
--- start.go
+++ start.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"github.com/Pallinder/go-randomdata"
 )
 
 func main() {
@@ -13,11 +14,11 @@ func main() {
 		fmt.Println(i)
 		time.Sleep(1 * time.Second)
 	}
-	
 	var input string
-	fmt.Println("[ golang ] と入力してください")
+	word := randomdata.Adjective()
+	fmt.Printf("[ %s ] と入力してください\n",word)
 	fmt.Scan(&input)
-	if input == "golang"{
+	if input == word{
 		fmt.Println("正解！")
 	} else {
 		fmt.Println("不正解...")
