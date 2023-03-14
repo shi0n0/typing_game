0b61b2746c0a2a9525ff0eaf379ecb93e3ce1981
shi0n0 <shiono.yui1080@gmail.com>
2023-03-14 16:18:29 +0900

    問題数表示

--- start.go
+++ start.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"time"
+
 	"github.com/Pallinder/go-randomdata"
 )
 
 func main() {
-	fmt.Println("3秒後に開始します")
-	time.Sleep(2 * time.Second)
-
 	for i := 3; i >= 1; i--{
 		fmt.Println(i)
 		time.Sleep(1 * time.Second)
@@ -19,7 +17,7 @@ func main() {
 	for e := 1; e <= 3; e++ {
 		var input string
 		word := randomdata.Adjective()
-		fmt.Printf("[ %s ] と入力してください\n",word)
+		fmt.Printf("[問題%d] %s と入力してください\n",e,word)
 		fmt.Scan(&input)
 		if input == word{
 			fmt.Println("正解！")
